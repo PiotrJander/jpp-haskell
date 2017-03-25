5a89f259b8b0c6c53526b01a569533cc5988dd23
PiotrJander <pjander2@gmail.com>
2017-03-25 16:15:01 +0100

    Lab monady 2, problem 2a

--- src/Monads.hs
+++ src/Monads.hs
@@ -18,3 +18,43 @@ allCombinations (xs:xss) = do
     y <- xs
     ys <- allCombinations xss
     return (y : ys)
+
+
+-- problem 2
+
+data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Eq, Ord, Show)
+
+renumber_ :: Tree a -> Tree Int
+renumber_ Empty = Empty
+renumber_ t = renumber_' 0 t
+
+renumber_' :: Int -> Tree a -> Tree Int
+renumber_' _ Empty = Empty
+renumber_' i (Node _ t1 t2) = Node i (renumber_' (i+1) t1) (renumber_' (i+1) t2)
+
+t :: Tree Int
+t = Node 0 (Node 0 Empty Empty) (Node 0 Empty (Node 0 Empty Empty))
+
+{-
+now let's try to use the reader Monad
+-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
