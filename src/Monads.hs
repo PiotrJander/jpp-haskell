916494c415ed8fe0c7d996c8e5790cb800a950bb
PiotrJander <pjander2@gmail.com>
2017-03-25 18:29:17 +0100

    Lab monady 2, problem 2

--- src/Monads.hs
+++ src/Monads.hs
@@ -1,5 +1,8 @@
 module Monads where
 
+import qualified Data.Map.Strict as M
+import Control.Monad
+import Data.Maybe
 
 -- problem 1
 
@@ -35,23 +38,54 @@ renumber_' i (Node _ t1 t2) = Node i (renumber_' (i+1) t1) (renumber_' (i+1) t2)
 t :: Tree Int
 t = Node 0 (Node 0 Empty Empty) (Node 0 Empty (Node 0 Empty Empty))
 
-{-
-now let's try to use the reader Monad
--}
+
+-- renumber :: Tree a -> Tree Int
+-- renumber
 
 
+type Var = String
+type Env = M.Map Var Int
+
+data Exp = EInt Int
+     | EOp  Op Exp Exp
+     | EVar Var
+     | ELet Var Exp Exp  -- let var = e1 in e2
+
+data Op = OpAdd | OpMul | OpSub
+
+-- | We could have an error when a variable not in the env, but we ignore this
+evalExp :: Exp -> Int
+evalExp exp = evalExp' exp M.empty
+
+-- evalExp' (EOp OpAdd e1 e2) = do
+--     fe1 <- evalExp' e1
+--     fe2 <- evalExp' e2
+--     return $ fe1 + fe2
+
+evalExp' :: Exp -> Env -> Int
+evalExp' (EInt i) = return i
+evalExp' (EOp OpAdd e1 e2) = liftM2 (+) (evalExp' e1) (evalExp' e2)
+evalExp' (EOp OpMul e1 e2) = liftM2 (*) (evalExp' e1) (evalExp' e2)
+evalExp' (EOp OpSub e1 e2) = liftM2 (-) (evalExp' e1) (evalExp' e2)
+evalExp' (EVar var) = \env -> let (Just value) = M.lookup var env in value
+evalExp' (ELet var e1 e2) = do
+    value <- evalExp' e1
+    evalExp' e2 . M.insert var value
 
 
-
-
-
-
-
-
-
-
-
-
+--
+--      let x =
+--          let y = 6 + 9
+--          in y - 1
+--      in x * 3
+--
+-- ==>  42
+--
+e1 = ELet "x" (ELet "y" (EOp OpAdd (EInt 6) (EInt 9))
+                      (EOp OpSub y (EInt 1)))
+                (EOp OpMul x (EInt 3))
+    where x = EVar "x"
+          y = EVar "y"
 
 
 
